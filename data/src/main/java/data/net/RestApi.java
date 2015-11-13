259425969b532cacb025715b5fa11dbba5427535
Victor <victor@refineriaweb.com>
2015-11-13 17:48:20 +0100

    added copyright

--- data/src/main/java/data/net/RestApi.java
+++ data/src/main/java/data/net/RestApi.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2015 RefineriaWeb
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package data.net;
 
 import java.util.List;
@@ -8,6 +24,7 @@ import retrofit.http.GET;
 import retrofit.http.Headers;
 import retrofit.http.Path;
 import rx.Observable;
+
 
 public interface RestApi {
     String URL_BASE = "https://api.github.com";
